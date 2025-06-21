9b49df8deef35e400350cb2de6b43c4d31f3dd2a
Meomeo12453 <nguoidentudaohoang@gmail.com>
2025-06-21 18:25:52 +0700

    app.py

--- app.py
+++ app.py
@@ -286,7 +286,7 @@ downloaded = st.download_button(
     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 )
 if downloaded:
-    st.toast("✅ Đã tải xuống!", icon="✅")
+    st.toast("Đã tải xuống!", icon="✅")
 
 
 st.markdown("<hr style='margin:10px 0 20px 0;border:1px solid #EEE;'>", unsafe_allow_html=True)
